3280de5d8268b7543945cf76b0b9acbf12fabf32
Travis Collins <trc229@gmail.com>
2018-09-21 09:33:57 -0400

    Implement sequelize for create and update methods

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -10,27 +10,33 @@ exports.index = (req, res) => {
     });
 };
 
+exports.createBurger = (req, res) => {
+  db.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured
+  })
+  .then(result => {
+    res.json({ id: result.insertId })
+  });
+}
+
+exports.updateBurger = (req, res) => {
+  const condition = `id = ${req.params.id}`;
+  console.log('condition', req.params.id);
+
+  db.Burger.update({
+    devoured: req.body.devoured,
+  }, {
+    where: {
+      id: 1 
+    }
+  })
+  .then(result => {
+    res.json(result);
+  });
+}
+
 /*
-router.get('/', (req, res) => {
-    burger.selectAll(data => {
-        const hbsObj = {
-            burgers: data
-        };
-        console.log(hbsObj);
-        res.render('index', hbsObj);
-    });
-});
-
-router.post('/api/burgers', (req, res) => {
-    burger.insertOne([
-        'burger_name', 'devoured'
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], result => {
-        res.json({ id: result.insertId });
-    });
-});
-
 router.put('/api/burgers/:id', (req, res) => {
     const condition = `id = ${req.params.id}`;
 
--- routes/burger.js
+++ routes/burger.js
@@ -6,4 +6,8 @@ const burgerController = require('../controllers/burgerController');
 
 router.get('/', burgerController.index);
 
+router.post('/api/burgers', burgerController.createBurger);
+
+router.put('/api/burgers/:id', burgerController.updateBurger);
+
 module.exports = router;
