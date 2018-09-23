6ee86a13853a33d33d4912fe7abfd4fd9864ccc8
Travis Collins <trc229@gmail.com>
2018-09-23 04:54:18 -0400

    Refactor to use node script entrypoint

---
+++ app.js
@@ -0,0 +1,34 @@
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const logger = require('morgan');
+const burgerRouter = require('./routes/burger');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', burgerRouter);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
