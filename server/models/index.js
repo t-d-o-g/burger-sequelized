6781b34aff3a8d64d248b72b7c399c06a6e9f2b2
Travis Collins <trc229@gmail.com>
2018-09-23 07:50:50 -0400

    ESLint fixes

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
-const db = {}; 
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
